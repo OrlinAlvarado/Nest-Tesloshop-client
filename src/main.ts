a73dcde1d896b38dd9b1fd6a19fca057ed7ff8b8
Orlin Alvarado <oalvaradomorales@gmail.com>
2022-09-15 14:29:49 +0000

    Seccion 15 finalizada

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import './style.css'
+import { connectToServer } from './socket-client';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h2>Web Socket - Client</h2>
+
+    <input id="jwt-token" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+
+    <br />
+    <span id="server-status">offline</span>
+
+    <ul id="clients-ul"></ul>
+
+    <form id="message-form">
+      <input placeholder="message" id="message-input" />
+    </form>
+  </div>
+
+  <h3>Messages</h3>
+  <ul id="messages-ul"></ul>
+`
+
+// connectToServer();
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+  if( jwtToken.value.trim().length <= 0) return alert('No valid token');
+
+  connectToServer(jwtToken.value);
+})
